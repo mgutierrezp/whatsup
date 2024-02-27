53c9ebdc2e3ba1863c1f2f38270dcdd270113e3e
mgutierrezp <mgutierrez@gmail.com>
2024-02-27 17:01:26 +0100

    first commit 1.0a

---
+++ README.md
@@ -0,0 +1,119 @@
+INTRODUCTION
+==============
+
+Search among your favorites astronomical objects and figure out at a glance which of them are visible just right now, at midnight, or whatever date and time you want. Show only those which an altitude greater than a specified value, or those far away from the moon some specified degrees. Have a more realistic view and let whatsup to perform a tour in Stellarium with the selected objects
+
+
+QUICK START
+==============
+
+*** precompiled binaries available for linux and windows ***
+
+*** just copy the corresponding binary from 'binaries' folder ***
+
+*** copy the conf file 'whatsup.config.xml' alongside the executable. Customize as needed  ***
+
+
+
+
+*** manual installation below: ***
+
+- Install Python >= 3.12
+
+- Create a new virtual environment and activate it (ensure you are executing python v3 and not v2!):
+	$ python -m venv whatsup_venv
+	# linux users:
+		$ source whatsup_env/bin/activate
+	# windows users:
+		# if using powershell
+			$ whatsup_env\Scripts\Activate.ps1
+		# if using a regular command prompt
+			$ whatsup_env\Scripts\activate.bat
+
+- Install python libraries within your new virtual environment
+	$ pip install -r requirements.txt
+
+
+
+USAGE
+==============
+options:
+  -h, --help            show this help message and exit
+  -v                    write some debug info. Optional
+  -V, --version         show program's version number and exit
+  --objects OBJECTS     Mandatory. Text file with objects or a comma separated list
+  --datetime DATETIME, --dt DATETIME
+                        Optional. Observation date and time. Multiple formats accepted. See dateutil.parser python library. A common format is "YYYY-MM-DD HH:MM". Default: astronomical twilight or current time if later
+  --minalt MINALT       Optional. Minimum altitude ([0-90] degrees). Default 0
+  --moon-separation MOON_SEPARATION, --ms MOON_SEPARATION
+                        Optional. Minimum separation to the moon ([0-360] degrees). Default 0
+  --stellarium-tour, --st
+                        Optional. Perform a stellarium tour
+
+
+--objects
+	simply specify the desired list of objects of your choice. You can type a comma-separated list of objects. Easier, you can specify the name of a text file containing such objects, one per line. Take care to quote the argument in spaces are present.
+	Examples:
+		whatsup --objects M31,M42,NGC1499
+		whatsup --objects ./myObjectsFile.txt
+		whatsup --objects "M 31, M 42"
+
+--datetime, --dt DATETIME
+	observation date and time. Have a look to supported formats: https://dateutil.readthedocs.io/en/stable/parser.html. If not specified, next astronomical twilight is automatically set, or the current time if later
+	Normally it should be enough with "YYYY-MM-DD HH:MM" or simply "HH:MM"
+	If format "HH:MM" is specified, an extra day will be added if the time is after midnight.
+	Examples:
+		whatsup --objects M31.M42 --dt "2024-02-27 21:00"
+		whatsup --objects M31,M42 --dt 01:00       # will be translated to "2024-02-28 01:00"
+
+--minalt MINALT
+	filter by objects with an altitude higher than MINALT at the specified DATETIME
+
+--moon-separation, --ms MOON_SEPARATION
+	filter by objects that are more than MOON_SEPARATION degrses from the moon at the specified DATETIME
+
+--stellarium, --st
+	if set, loops over the results and focus each object on Stellarium. You need to setup Stellarium and whatsup config file. See below
+
+
+This is a very typical execution example:
+
+	whatsup.py --objects=M31,M42,NGC1499 --dt "20:00"  --ms 50 --minalt 40 --st
+
+	2024/02/27 16:49:11 - INFO - --- STARTING ---
+	2024/02/27 16:49:11 - INFO - running whatsup version 1.0a
+	2024/02/27 16:49:12 - INFO - location: Madrid (Europe/Madrid)
+	2024/02/27 16:49:12 - INFO - date/time: 2024-02-27 20:00:00+01:00
+	2024/02/27 16:49:12 - INFO - moon illumination is 91%
+	2024/02/27 16:49:12 - INFO - searching
+
+	Starting stellarium tour
+
+	object    rise time (above horizon)    meridian side    set time (below 40 deg)    moon separation    altitude @time
+	--------  ---------------------------  ---------------  -------------------------  -----------------  ----------------
+	M42       2024-02-27 14:42:05+01:00    east             2024-02-27 21:49:50+01:00  108d43m01.62s      43d54m27.85s
+	NGC1499   2024-02-27 10:16:37+01:00    west             2024-02-27 23:11:18+01:00  126d54m48.23s      76d00m08.51s
+	M31       2024-02-28 06:14:00+01:00    west             2024-02-26 20:06:17+01:00  144d26m54.70s      40d24m23.34s
+
+
+
+
+
+CONFIG FILE
+==============
+This is an example of the config file:
+
+<?xml version="1.0" encoding="UTF-8"?>
+<config>
+	<general>
+		<location name="Madrid" timezone="Europe/Madrid" />
+		<stellarium host="127.0.0.1" scheme="http" port="8090" />
+	</general>
+</config>
+
+This file must reside on the same folder as whatsup application.
+
+You will need to change the "location name" value and set the one you are observing from. Change the "timezone" accordingly.
+
+If you want to whatsup to perform a Stellarium tour (see parameter --st above) you need to enable the Remote Control within Stellarium: https://stellarium.org/doc/0.16/remoteControlDoc.html
+Usually the default host, scheme and port should work. Change if needed
---
+++ whatsup.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python3
+
+VERSION="1.0a"
+
+import sys,argparse,logging,os,traceback,xmltodict,pytz,urllib,ephem,platform,dateutil.parser,astropy,astroplan,warnings
+
+if platform.system().lower() == "linux":
+	from simple_term_menu import TerminalMenu
+
+from tabulate import tabulate
+import datetime as dt
+from pathlib import Path
+from astropy import units as u                                                                                                                                                                                                        
+from astropy.coordinates import SkyCoord, AltAz, Angle
+from functools import cmp_to_key
+from tqdm import tqdm 
+from astroplan import Observer, FixedTarget
+from astropy.coordinates import EarthLocation
+from astropy.time import Time
+
+warnings.filterwarnings('ignore')
+
+SCRIPT_DIR=Path(sys.argv[0]).parent
+CONFIG_FILE=SCRIPT_DIR.joinpath(Path(Path(sys.argv[0]).stem).with_suffix(".config.xml"))
+SCRIPT_NAME = Path(sys.argv[0]).stem
+
+
+def loadConfig():
+	if not os.path.exists(CONFIG_FILE):
+		logger.critical("config file not found: %s" % CONFIG_FILE)
+		sys.exit(1)
+
+	try:
+		with open(CONFIG_FILE, "r") as f:
+			p=xmltodict.parse(f.read(), force_list=('zone'))
+			
+			EarthLocation.of_address(p["config"]["general"]["location"]["@name"])
+			pytz.timezone(p["config"]["general"]["location"]["@timezone"])
+
+	except Exception as e:
+		logger.critical("error while parsing config file. Find below the original exception; most likely due to a syntax error in your config file")
+		traceback.print_exc()
+		sys.exit(1)
+
+	return p
+
+def checkMoonSeparation(sep):
+	int(sep)
+	if int(sep) < 0 or int(sep) > 360:
+		raise ValueError
+	else:
+		return sep
+
+def checkAltitude(sep):
+	int(sep)
+	if int(sep) < 0 or int(sep) > 90:
+		raise ValueError
+	else:
+		return sep
+
+def checkDatetime(d):
+	dateutil.parser.parse(d)
+	return d
+
+
+def parse_options():
+	usage = "%(prog)s"
+
+	parser = argparse.ArgumentParser(usage=usage)
+	parser.add_argument("-v", dest="verbose", action="store_true", help="write some debug info. Optional")
+	parser.add_argument("-V", "--version", action="version", version=VERSION)
+	parser.add_argument("--objects", action="store", help="Mandatory. Text file with objects or a comma separated list", required=True)
+	parser.add_argument("--datetime", "--dt", type=checkDatetime, action="store", help="Optional. Observation date and time. Multiple formats accepted. See dateutil.parser python library. A common format is \"YYYY-MM-DD HH:MM\". Default: astronomical twilight or current time if later ", required=False)
+	parser.add_argument("--minalt", action="store", type=checkAltitude, help="Optional. Minimum altitude ([0-90] degrees). Default 0", default=0)
+	parser.add_argument("--moon-separation", "--ms", action="store", type=checkMoonSeparation, help="Optional. Minimum separation to the moon ([0-360] degrees). Default 0", default=0)
+	parser.add_argument("--stellarium-tour", "--st", action="store_true", help="Optional. Perform a stellarium tour", default=False)
+	
+
+	return parser
+
+def setup_custom_logger(name, options):
+    logger = logging.getLogger(name)
+
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+
+    if options.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def setStellariumFocus(target):
+	urllib.request.urlopen("%s://%s:%s/api/main/focus" % (config["config"]["general"]["stellarium"]["@scheme"],config["config"]["general"]["stellarium"]["@host"],config["config"]["general"]["stellarium"]["@port"]),data=b"target=%b" % bytes(target,'utf-8'))
+
+def setStellariumFocus2(target):
+	urllib.request.urlopen("%s://%s:%s/api/main/focus" % (config["config"]["general"]["stellarium"]["@scheme"],config["config"]["general"]["stellarium"]["@host"],config["config"]["general"]["stellarium"]["@port"]),data=b"target=%b" % bytes(target.split()[0],'utf-8'))
+	
+def setStellariumTime(t):
+	urllib.request.urlopen("%s://%s:%s/api/main/time" % (config["config"]["general"]["stellarium"]["@scheme"],config["config"]["general"]["stellarium"]["@host"],config["config"]["general"]["stellarium"]["@port"]),data=b"time=%b" % bytes(t,'utf-8'))
+
+
+def checkStellariumStatus():
+	try:
+		urllib.request.urlopen("%s://%s:%s/api/main/status" % (config["config"]["general"]["stellarium"]["@scheme"],config["config"]["general"]["stellarium"]["@host"],config["config"]["general"]["stellarium"]["@port"]))
+	except Exception as e:
+		return e
+	
+	return True
+	
+def getTransit(stime, coords):
+	return observer.target_meridian_transit_time(stime, coords).to_datetime(pytz.timezone(tz))
+
+def mikSort(x, y):
+	if x["meridian_side"] == y["meridian_side"]:
+		if x["meridian_side"] == "east":
+			return -1 if x["coordsAltAz"].alt < y["coordsAltAz"].alt else 1
+		else:
+			return -1 if x["coordsAltAz"].alt > y["coordsAltAz"].alt else 1
+	
+	return -1 if x["meridian_side"] == "east" else 1
+
+
+#####################################################################################
+
+
+parser = parse_options()
+options = parser.parse_args()
+
+logger = setup_custom_logger('root', options)
+
+logger.info("--- STARTING ---")
+logger.info("running %s version %s" % (SCRIPT_NAME, VERSION))
+
+config=loadConfig()
+
+
+location=config["config"]["general"]["location"]["@name"]
+tz=config["config"]["general"]["location"]["@timezone"]
+logger.info("location: %s (%s)" % (location, tz))
+
+observatory_location = EarthLocation.of_address(location)
+observer = Observer(location=observatory_location, name="Observer")
+
+if not options.datetime:
+	now=dt.datetime.now()
+	stime=now.astimezone(pytz.timezone(tz))
+	twilight=observer.twilight_evening_astronomical(astropy.time.Time(now.replace(hour=12))).to_datetime(pytz.timezone(tz))
+	stime=twilight if stime < twilight else stime
+	stime = stime if stime != "[--]" else dt.datetime.now().astimezone(pytz.timezone(tz))
+else:
+	stime=dateutil.parser.parse(options.datetime).astimezone(pytz.timezone(tz))
+	stime = stime if stime > dt.datetime.now().astimezone(pytz.timezone(tz)) else  stime + dt.timedelta(days=1)
+	
+logger.info("date/time: %s" % stime)	
+illum=round(astroplan.moon_illumination(astropy.time.Time(stime))*100)
+logger.info("moon illumination is %s%%" % illum)
+	
+if os.path.exists(options.objects):
+	with open(options.objects) as f:
+		objects = [s.strip() for s in f.readlines()]
+else:
+	objects=options.objects.split(",")
+
+visibleObjects=[]
+moonAltAz = observer.moon_altaz(stime)
+moonAlt, moonAz = moonAltAz.alt, moonAltAz.az
+logger.debug("moon AltAz coords: alt %s az %s" %(moonAlt, moonAz))
+
+objects=[] if len(objects) ==1 and objects[0]=='' else objects
+logger.info("searching")
+bar=tqdm(total=len(objects))
+nonResolvedObjects=[]
+
+for oobject in objects:
+	bar.update()
+	logger.debug("object: %s" % oobject)
+	try:
+		try:
+			coords=SkyCoord.from_name(oobject)
+		except astropy.coordinates.name_resolve.NameResolveError:
+			nonResolvedObjects.append(oobject)
+		coordsAltAz = coords.transform_to(AltAz(obstime=stime,location=observatory_location))
+		moonSeparation=moonAltAz.separation(coords)
+		logger.debug(" alt: %s" % coordsAltAz.alt)
+		logger.debug(" az: %s"% coordsAltAz.az)
+		logger.debug(" moon separation: %s" % moonSeparation)
+		if coordsAltAz.alt > Angle(str(options.minalt)+"d") and moonSeparation > Angle(str(options.moon_separation)+"d"):
+			transit = getTransit(stime, coords)
+			meridianside = "west" if transit < stime else "east"
+			logger.debug(" object meridian side: %s" % meridianside)
+			logger.debug(" including")
+			target_coordinates = SkyCoord.from_name(oobject)
+			target = FixedTarget(coord=target_coordinates, name=oobject)
+			rise_time = observer.target_rise_time(astropy.time.Time(stime), target)
+			set_time = observer.target_set_time(astropy.time.Time(stime), target, horizon=astropy.units.Quantity(options.minalt, unit='degree'))
+			visibleObjects.append({"object": oobject, "rise_time": rise_time, "meridian_side": meridianside, "coords": coords, "coordsAltAz": coordsAltAz, "transit": transit, 'moon_separation': moonSeparation, "set_time": set_time })
+	except Exception as e:
+		logger.warning(e)
+		pass
+
+bar.close()
+
+if len(nonResolvedObjects) > 0: logger.warning("the following objects names could not be resolved: %s" % nonResolvedObjects)
+	
+print()
+visibleObjects=sorted(visibleObjects, key=cmp_to_key(mikSort))
+
+headers=["object","rise time (above horizon)", "meridian side", "set time (below %s deg)" % options.minalt, "moon separation", "altitude @time"]
+t=[]
+
+for visibleObject in visibleObjects:
+	sanitizedRiseTime = visibleObject["rise_time"].to_datetime(pytz.timezone(tz)).isoformat(timespec='seconds', sep=" ") if not isinstance(visibleObject["rise_time"].to_datetime(pytz.timezone(tz)), astropy.utils.masked.core.MaskedNDArray) else "always above horizon"
+	sanitizedSetTime = visibleObject["set_time"].to_datetime(pytz.timezone(tz)).isoformat(timespec='seconds', sep=" ") if not isinstance(visibleObject["set_time"].to_datetime(pytz.timezone(tz)), astropy.utils.masked.core.MaskedNDArray) else "--"
+	t.append([visibleObject["object"], sanitizedRiseTime, visibleObject["meridian_side"], sanitizedSetTime, visibleObject['moon_separation'].to_string(precision=2), visibleObject["coordsAltAz"].alt.to_string(precision=2)])
+
+if len(t) > 0:
+	print()
+
+	if options.stellarium_tour:
+		st=checkStellariumStatus()
+		if st is not True:
+			logger.critical("stellarium is not available. Please check host and port config and make sure the 'Remote Control' plugin in Stellarium is enabled and properly configured. Original exception follows:")
+			print(st)
+			sys.exit(0)
+		print()
+		print("Starting stellarium tour")
+		print()
+		
+		setStellariumTime(str(ephem.julian_date(ephem.Date(stime))))
+		
+		if platform.system().lower() == "linux":
+			tt=tabulate(t, headers=headers).split("\n")
+			print("  "+tt[0])
+			print("  "+tt[1])
+			terminal_menu = TerminalMenu(tt[2:], preview_command=setStellariumFocus2, preview_size=0.1, clear_menu_on_exit = False)
+			menu_entry_index = terminal_menu.show()
+		else:
+			i=1
+			print(tabulate(t, headers = headers))
+			print()
+			print("press ENTER for next object. CTRL-C to exit")
+			while True:
+				print()
+				print("Iteration #%s" % i)
+				i+=1
+				for visibleObject in visibleObjects:
+					print(visibleObject["object"])
+					setStellariumFocus(visibleObject["object"])
+					input()
+	else:
+		print(tabulate(t, headers = headers))
+else:
+	print("no results")
+
+print()
